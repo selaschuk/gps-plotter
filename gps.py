26300b5d24243454e799866f19001f4ea293e820
Stephen Elaschuk <selaschuk@users.noreply.github.com>
2016-02-09 11:44:04 -0700

    Update gps.py
    move to threaded because requests were queuing more than 5 deep and connections were being refused.

--- gps.py
+++ gps.py
@@ -1,25 +1,22 @@
+import socket
+import threading
 import SocketServer
 import pynmea2
 import json
 import datetime
 
-json_data = {}
+gpsData = {}
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
+        global gpsData
         # self.request is the TCP socket connected to the client
-        global json_data
         self.data = self.request.recv(1024).strip()
         rawdata = self.data
-        #uncomment following to debug
-        #print self.data 
+        #uncomment following lines to debug
+        #print self.data
+        #print "{} wrote:".format(self.client_address[0])
         if 'GET /gps.json HTTP/1.1' in rawdata:
             response_proto = 'HTTP/1.1'
             response_status = '200'
@@ -27,7 +24,7 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 
             response_headers = {
                 'Access-Control-Allow-Origin': '*',
-                'Content-Length': len(json.dumps(json_data)),
+                'Content-Length': len(json.dumps(gpsData)),
                 'Content-Type': 'text/html; encoding=utf8',
                 'Connection': 'close',
             }
@@ -35,16 +32,15 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
             response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.iteritems())
 
             self.request.send('%s %s %s' % (response_proto, response_status, response_status_text))
-            self.request.send('\n') # to separate response from headers
+            self.request.send('\n')
             self.request.send(response_headers_raw)
             self.request.send('\n') # to separate headers from body
-            self.request.send(json.dumps(json_data))
+            self.request.send(json.dumps(gpsData))
 
         elif 'HTTP/1.' in rawdata:
             response_proto = 'HTTP/1.1'
             response_status = '404'
             response_status_text = 'Not Found'
-
             self.request.send('%s %s %s' % (response_proto, response_status, response_status_text))            
 
         else:
@@ -59,22 +55,28 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
                     #GGA sentence detected, collect position data
                     updateTime = datetime.datetime.combine(datetime.datetime.utcnow().date(), parsedNema.timestamp)
                     ggaData = {"info": gpsId, "lat": parsedNema.latitude, "lng": parsedNema.longitude, "updt": updateTime.isoformat()}
-                    json_data.setdefault(gpsId, ggaData)
-                    json_data[gpsId].update(ggaData)
+                    gpsData.setdefault(gpsId, ggaData)
+                    gpsData[gpsId].update(ggaData)
                 elif isinstance(parsedNema, pynmea2.types.talker.VTG):
                     #VTG sentence detected, collect speed data
                     vtgData = {"info": gpsId, "spd": parsedNema.spd_over_grnd_kmph}
-                    json_data.setdefault(gpsId, vtgData)
-                    json_data[gpsId].update(vtgData)
+                    gpsData.setdefault(gpsId, vtgData)
+                    gpsData[gpsId].update(vtgData)
 
-        self.request.close()
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
 
 if __name__ == "__main__":
+    # Bind to port 8889
     HOST, PORT = "0.0.0.0", 8889
 
-    # Create the server, binding to localhost on port 8889
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
+    # Start a thread with the server -- that thread will then start one
+    # more thread for each request
+    server_thread = threading.Thread(target=server.serve_forever)
+    # Exit the server thread when the main thread terminates
+    server_thread.daemon = True
+    server_thread.start()
+
     server.serve_forever()
